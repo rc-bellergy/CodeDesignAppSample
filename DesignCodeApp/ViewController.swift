78ead8f429cc69ccaf134c84bef6c3793e71bc09
Michael <michael@designquest.com.hk>
2019-08-26 16:16:32 +0800

    Initial Commit

---
+++ DesignCodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignCodeApp
+//
+//  Created by Michael on 26/8/2019.
+//  Copyright © 2019 Design Quest Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
