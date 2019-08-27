b9cb4b7982a99b8ba89f881ba16cd07dad9ca34d
Michael <michael@designquest.com.hk>
2019-08-27 15:27:08 +0800

    init project

---
+++ DesignCodeApp/SectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SectionViewController.swift
+//  DesignCodeApp
+//
+//  Created by Michael on 27/8/2019.
+//  Copyright © 2019 Design Quest Limited. All rights reserved.
+//
+
+import UIKit
+
+class SectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
