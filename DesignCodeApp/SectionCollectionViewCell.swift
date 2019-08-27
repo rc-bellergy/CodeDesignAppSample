bd56f484bf343fe94cb426953962ad143eb28944
RC Bellergy <rc@bellergy.com>
2019-08-27 22:04:49 +0800

    Add Static Data

---
+++ DesignCodeApp/SectionCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by Bellergy on 27/8/2019.
+//  Copyright © 2019 Design Quest Limited. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+}
