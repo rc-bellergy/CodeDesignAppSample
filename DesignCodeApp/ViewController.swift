44ad9f8342a792abc1831afac73c904f666c1289
Michael <michael@designquest.com.hk>
2019-08-27 17:59:26 +0800

    Add collection view

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var deviceImageView: UIImageView!
     @IBOutlet weak var playVisualView: UIVisualEffectView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var heroView: UIView!
+    @IBOutlet weak var bookView: UIView!
+    @IBOutlet weak var chapterCollectionView: UICollectionView!
+    
     @IBAction func playButtonTapped(_ sender: Any) {
         let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
         let url = URL(string: urlString)
@@ -25,10 +31,13 @@ class ViewController: UIViewController {
         }
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        scrollView.delegate = self
+        chapterCollectionView.delegate = self
+        chapterCollectionView.dataSource = self
         
         titleLabel.alpha = 0
         deviceImageView.alpha = 0
@@ -40,7 +49,32 @@ class ViewController: UIViewController {
             self.playVisualView.alpha = 1
         }
     }
-
-
+    
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        print(scrollView)
+        let offsetY = scrollView.contentOffset.y
+        if (offsetY < 0) {
+            heroView.transform = CGAffineTransform(translationX: 0, y: offsetY)
+            titleLabel.transform = CGAffineTransform(translationX: 0, y: -offsetY/2)
+            playVisualView.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
+            backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectionCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
